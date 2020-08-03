d2c270705511829b05e19219b6d06030a0dbacd8
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-08-03 18:21:49 +0300

    hotfix: добавить if условие в функцию saveForm в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,29 @@ let profileEditButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileCaption = profile.querySelector('.profile__caption');
 
-function togglePopUp() {
-  popUp.classList.toggle('popup_opened');
+function openPopUp() {
+  popUp.classList.add('popup_opened');
   inputName.focus();
   inputName.value = profileName.textContent;
   inputCaption.value = profileCaption.textContent;
 }
 
+function closePopUp() {
+  popUp.classList.remove('popup_opened');
+}
+
 function saveForm(evt) {
   evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileCaption.textContent = inputCaption.value;
-  togglePopUp();
+  if (profileName.textContent !== inputName.value) {
+    profileName.textContent = inputName.value;
+  }
+  if (profileCaption.textContent !== inputCaption.value) {
+    profileCaption.textContent = inputCaption.value;
+  }
+  closePopUp();
 }
 
 
-profileEditButton.addEventListener('click', togglePopUp);
-popUpCloseButton.addEventListener('click', togglePopUp);
+profileEditButton.addEventListener('click', openPopUp);
+popUpCloseButton.addEventListener('click', closePopUp);
 editForm.addEventListener('submit', saveForm);
