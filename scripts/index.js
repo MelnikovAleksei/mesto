8327d850311fa265c76e6d033e671c160a40076e
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-08-18 11:26:44 +0300

    feat: создать функцию добавления фотографии

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let popUp = document.querySelector('.popup');
 let popUpCloseButton = popUp.querySelector('.popup__close-button');
 
@@ -9,6 +36,18 @@ let profile = document.querySelector('.profile');
 let profileEditButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileCaption = profile.querySelector('.profile__caption');
+
+let photosList = document.querySelector('.photos__list');
+
+function addPhotosElement(name, link) {
+  const photosElement = document.querySelector('#photos-element').content;
+  const photosCard = photosElement.cloneNode(true);
+  console.log(photosCard);
+  photosCard.querySelector('.photos__image').src = link;
+  photosCard.querySelector('.photos__image').alt = 'фотография' + name;
+  photosCard.querySelector('.photos__figcaption').textContent = name;
+  photosList.append(photosCard);
+}
 
 function openPopUp() {
   inputName.value = profileName.textContent;
