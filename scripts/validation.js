6970471c6d39369b6d84a86482705d87007e14de
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-08-26 10:49:03 +0300

    feat: создать объект validationSettings

---
+++ scripts/validation.js
@@ -0,0 +1,9 @@
+const validationSettings = {
+  formSelector: '.form',
+  fieldsetSelector: '.form__fieldset',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_inactive',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_active',
+}
