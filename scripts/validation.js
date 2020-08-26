e9845f9acd8b34e983125aca5ca4c6e7fe4d9617
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-08-26 11:18:08 +0300

    fix: исправить параметры функции enableValidation

--- scripts/validation.js
+++ scripts/validation.js
@@ -8,26 +8,36 @@ const validationSettings = {
   errorClass: 'form__input-error_active',
 }
 
+function checkInputValidity(formElement, inputElement) {
+  console.log(formElement);
+  console.log(inputElement);
+}
+
+function toggleSubmitButtonState(inputElement, formSubmitButton) {
+  console.log(inputElement);
+  console.log(formSubmitButton);
+}
+
 function setEventListeners(formElement) {
   const formInputs = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
   const formSubmitButton = formElement.querySelector(validationSettings.submitButtonSelector);
-  /* toggleSubmitButtonState */
+  toggleSubmitButtonState(formInputs, formSubmitButton);
   formInputs.forEach(inputElement => {
-    inputElement.addEventListener('input', function (evt) {
-      /* checkInputValidity(formElement, inputElement); */
-      /* toggleSubmitButtonState(inputElement, formSubmitButton) */
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleSubmitButtonState(formInputs, formSubmitButton);
     })
   })
 }
 
 
 function enableValidation(settingsObject) {
-  const formsList = Array.from(document.querySelectorAll(validationSettings.formSelector));
+  const formsList = Array.from(document.querySelectorAll(settingsObject.formSelector));
   formsList.forEach(formElement => {
     formElement.addEventListener('submit', function (evt) {
       evt.preventDefault();
     })
-    const fieldsetList = Array.from(formElement.querySelectorAll(validationSettings.fieldsetSelector));
+    const fieldsetList = Array.from(formElement.querySelectorAll(settingsObject.fieldsetSelector));
     fieldsetList.forEach(fieldset => {
       setEventListeners(fieldset);
     })
