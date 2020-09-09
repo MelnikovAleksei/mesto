27d99d53b75af639bf8d7a91093787b306979d23
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-09-09 17:49:45 +0300

    feat: переименовать имена данный в Constructor класса Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -3,18 +3,18 @@ class Card {
     this._name = cardData.name;
     this._link = cardData.link;
     this._cardTemplateSelector = settings.templateSelector;
-    this._photoCardSelector = settings.photoCardSelector;
-    this._photoImageSelector = settings.photoImageSelector;
-    this._photoFigcaptionSelector = settings.photoFigcaptionSelector;
-    this._photoLikeButtonSelector = settings.photoLikeButtonSelector;
-    this._photoDeleteButtonSelector = settings.photoDeleteButtonSelector;
+    this._cardSelector = settings.photoCardSelector;
+    this._imageSelector = settings.photoImageSelector;
+    this._figcaptionSelector = settings.photoFigcaptionSelector;
+    this._likeButtonSelector = settings.photoLikeButtonSelector;
+    this._deleteButtonSelector = settings.photoDeleteButtonSelector;
   }
 
   _getTemplate() {
     return document
       .querySelector(this._cardTemplateSelector)
       .content
-      .querySelector(this._photoCardSelector)
+      .querySelector(this._cardSelector)
       .cloneNode(true);
   }
 
@@ -48,9 +48,9 @@ class Card {
   }
 
   _setEventListeners() {
-    this._cardElement.querySelector(this._photoLikeButtonSelector).addEventListener('click', this._like);
-    this._cardElement.querySelector(this._photoDeleteButtonSelector).addEventListener('click', this._delete);
-    this._cardElement.querySelector(this._photoImageSelector).addEventListener('click', this._openPopup);
+    this._cardElement.querySelector(this._likeButtonSelector).addEventListener('click', this._like);
+    this._cardElement.querySelector(this._deleteButtonSelector).addEventListener('click', this._delete);
+    this._cardElement.querySelector(this._imageSelector).addEventListener('click', this._openPopup);
     document.querySelector('.popup-photos__close-button').addEventListener('click', this._closePopup);
     document.querySelector('.popup-photos').addEventListener('click', (evt) => {
       if (evt.target.classList.contains('popup')) {
@@ -67,8 +67,8 @@ class Card {
   generateCard() {
     this._cardElement = this._getTemplate();
     this._setEventListeners();
-    this._cardElement.querySelector(this._photoImageSelector).src = this._link;
-    this._cardElement.querySelector(this._photoFigcaptionSelector).textContent = this._name;
+    this._cardElement.querySelector(this._imageSelector).src = this._link;
+    this._cardElement.querySelector(this._figcaptionSelector).textContent = this._name;
     return this._cardElement;
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -50,8 +50,6 @@ const renderCards = (cardsData) => {
   })
 }
 
-
-
 renderCards(initialCardsData)
 
 // =========================================================
