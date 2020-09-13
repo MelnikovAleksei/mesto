20586cfccbe9613cf3b2d2d7dddefa73a817d208
library129 <melnikov.aleksei.alexeyevich@gmail.com>
2020-09-13 16:22:20 +0300

    hotfix: создать файл с данными карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+import {initialCardsData} from './cardsData.js'
 import {Card} from './Card.js';
 import {FormValidator} from './FormValidator.js'
 
@@ -25,33 +26,6 @@ const validationSettings = {
   addCardButtonClass: 'profile__add-button',
   editProfileButtonClass: 'profile__edit-button',
 }
-
-const initialCardsData = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // selectors
 const photoTemplateSelector = '#photos-element';
