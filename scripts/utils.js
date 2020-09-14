31a605f32ad856b18e23b646913594b3da13d5aa
melnikov <melnikov.aleksei.alexeyevich@gmail.com>
2020-09-14 19:36:08 +0300

    hotfix: создать переменные для cелекторов в utils

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,20 +4,26 @@ const popupImage = popupPhotos.querySelector('.popup-photos__image');
 const popupFigcaption = popupPhotos.querySelector('.popup-photos__figcaption');
 const escapeKey = 'Escape';
 
+const photoFigureSelector = '.photos__figure';
+const photoImageSelector = '.photos__image';
+const photoFigcaptionSelector = '.photos__figcaption';
+
+const popupOpenedClass = 'popup_opened';
+
 const openPhotoPopup = (photoCardElement) => {
-  const figureElement = photoCardElement.querySelector('.photos__figure');
-  const imageElement = figureElement.querySelector('.photos__image');
-  const figcaptionElement = figureElement.querySelector('.photos__figcaption');
+  const figureElement = photoCardElement.querySelector(photoFigureSelector);
+  const imageElement = figureElement.querySelector(photoImageSelector);
+  const figcaptionElement = figureElement.querySelector(photoFigcaptionSelector);
   popupImage.src = imageElement.src;
   popupFigcaption.textContent = figcaptionElement.textContent;
-  popupPhotos.classList.add('popup_opened');
+  popupPhotos.classList.add(popupOpenedClass);
   setClosePhotoPopupEventListeners();
 }
 
 const closePhotoPopup = () => {
   popupImage.src = '';
   popupFigcaption.textContent = '';
-  popupPhotos.classList.remove('popup_opened');
+  popupPhotos.classList.remove(popupOpenedClass);
   popupPhotos.removeEventListener('click', popupTargetHandler);
   popupCloseButton.removeEventListener('click', closePhotoPopup);
   document.removeEventListener('click', escapeKeyHandler);
