2382a2c6d1624bc588a6e5761fbf6df6ba23fcca
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-09-28 15:08:26 +0300

    feat: install postcss-loader, cssnano, autoprefixer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import './pages/index.css';
+import './index.css';
 
-import {initialCardsData} from './components/cardsData.js'
-import {Section} from './components/Section.js';
-import {Card} from './components/Card.js';
-import {FormValidator} from './components/FormValidator.js';
-import {PopupWithImage} from './components/PopupWithImage.js';
-import {PopupWithForm} from './components/PopupWithForm.js';
-import {UserInfo} from './components/UserInfo.js';
+import {initialCardsData} from '../components/cardsData.js'
+import {Section} from '../components/Section.js';
+import {Card} from '../components/Card.js';
+import {FormValidator} from '../components/FormValidator.js';
+import {PopupWithImage} from '../components/PopupWithImage.js';
+import {PopupWithForm} from '../components/PopupWithForm.js';
+import {UserInfo} from '../components/UserInfo.js';
 
 // settings
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
@@ -16,23 +16,32 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
-        loader: 'file-loader'
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ],
       },
       {
         test: /\.html$/,
         loader: 'html-loader',
       },
       {
-        test: /\.css$/,
-        loader: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
+        loader: 'file-loader',
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: 'src/index.html'
     }),
     new MiniCssExtractPlugin()
   ]
-}
+};
